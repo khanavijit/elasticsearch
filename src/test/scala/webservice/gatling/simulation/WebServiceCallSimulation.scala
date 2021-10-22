7ff5d40637c8ad9d2d48abc776fdddf9820f9fcd
Avijit Khan <khanavijit@gmail.com>
2021-10-22 10:53:10 +0200

    added more performance test cases

--- src/test/scala/webservice/gatling/simulation/WebServiceCallSimulation.scala
+++ src/test/scala/webservice/gatling/simulation/WebServiceCallSimulation.scala
@@ -2,22 +2,39 @@ package webservice.gatling.simulation
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.language.postfixOps
 import scala.concurrent.duration._
+
+
+import java.util.Locale
 
 class WebServiceCallSimulation extends Simulation {
 
   val rampUpTimeSecs = 5
   val testTimeSecs = 20
   val noOfUsers = 10
-  val minWaitMs = 1000 milliseconds
-  val maxWaitMs = 3000 milliseconds
+  val minWaitMs = 50 milliseconds
+  val maxWaitMs = 1000 milliseconds
 
   val baseURL = "http://localhost:8080"
   val baseName = "/v1"
-  val requestName = baseName + "/search"
-  val scenarioName = baseName + "-searchById"
-  val URI = "/v1/search?attribute=id&value=67"
+  val requestName1 = baseName + "/searchById"
+  val requestName2 = baseName + "/searchBySubject"
+  val requestName3 = baseName + "/searchByPriority"
+  val requestName4 = baseName + "/searchByDescription"
+
+
+
+  val scenarioName1 = baseName + "-searchById"
+  val scenarioName2 = baseName + "-searchBySubject"
+  val scenarioName3 = baseName + "-searchByPriority"
+  val scenarioName4 = baseName + "-searchByDescription"
+
+  val URI1 = "/v1/search?attribute=id&value=67"
+  val URI2 = "/v1/search?attribute=subject&value=missing"
+  val URI3 = "/v1/search?attribute=priority&value=high"
+  val URI4 = "/v1/search?attribute=description&value=Glory"
 
   val httpConf = http
     .baseUrl(baseURL)
@@ -27,16 +44,44 @@ class WebServiceCallSimulation extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario(scenarioName)
+  val scn = scenario(scenarioName1)
     .during(testTimeSecs) {
       exec(
-        http(requestName)
-          .get(URI)
+        http(requestName1)
+          .get(URI1)
+          .check(status.is(200))
+      ).pause(minWaitMs, maxWaitMs)
+    }
+
+  val scn2 = scenario(scenarioName2)
+    .during(testTimeSecs) {
+      exec(
+        http(requestName2)
+          .get(URI2)
+          .check(status.is(200))
+      ).pause(minWaitMs, maxWaitMs)
+    }
+  val scn3 = scenario(scenarioName3)
+    .during(testTimeSecs) {
+      exec(
+        http(requestName3)
+          .get(URI3)
+          .check(status.is(200))
+      ).pause(minWaitMs, maxWaitMs)
+    }
+  val scn4 = scenario(scenarioName4)
+    .during(testTimeSecs) {
+      exec(
+        http(requestName4)
+          .get(URI4)
           .check(status.is(200))
       ).pause(minWaitMs, maxWaitMs)
     }
 
   setUp(
-    scn.inject(rampUsers(noOfUsers) during (rampUpTimeSecs))
+    scn.inject(rampUsers(noOfUsers) during (rampUpTimeSecs)) ,
+    scn2.inject(rampUsers(noOfUsers) during (rampUpTimeSecs)),
+    scn3.inject(rampUsers(noOfUsers) during (rampUpTimeSecs)),
+    scn4.inject(rampUsers(noOfUsers) during (rampUpTimeSecs))
   ).protocols(httpConf)
 }
