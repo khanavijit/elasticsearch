4aec45117d8c23c29b23e8ead5e2d18d7a5ee465
Avijit Khan <akh@yousee.sk>
2021-10-26 13:07:20 +0200

    Clean up

--- src/main/java/com/avijit/projects/elasticsearch/document/Ticket.java
+++ src/main/java/com/avijit/projects/elasticsearch/document/Ticket.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @Document(indexName = Indices.TICKET_INDEX)
+@Setting(shards = 3,replicas = 2)
 //@Mapping(mappingPath = "/elasticSearch.json")
 public class Ticket extends org.avijit.projects.generated.model.Ticket  {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
